68e8c5d6688751d5c26853bf2b0c6e0cb6a55d03
gergocsontos <gergo.csontos.123@gmail.com>
2021-12-14 14:44:10 +0100

    initialize project with Spring Boot\ add gitignore file

---
+++ src/main/java/com/codecool/mavens/ExpertFinderApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.mavens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/mavens/ExpertFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.mavens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
