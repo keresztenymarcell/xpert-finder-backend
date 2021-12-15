52b4806abea20385a95c655c2d98314af120ecec
gergocsontos <gergo.csontos.123@gmail.com>
2021-12-15 14:23:32 +0100

    add UserController start

---
+++ src/main/java/com/codecool/mavens/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.codecool.mavens.controller;
+
+import com.codecool.mavens.entity.User;
+import com.codecool.mavens.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable String id) {
+        return "";
+    }
+
+    @PostMapping("/{id}")
+    public String addNewUser(@RequestBody User user) {
+        return "";
+    }
+}
