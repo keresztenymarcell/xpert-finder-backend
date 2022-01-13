75b41d0d2a7744e1d76827d19305e6d006955954
gergocsontos <gergo.csontos.123@gmail.com>
2022-01-13 15:48:57 +0100

    feat: add dummy search-all-trial endpoint with 2 users

--- src/main/java/com/codecool/mavens/controller/UserController.java
+++ src/main/java/com/codecool/mavens/controller/UserController.java
@@ -25,20 +25,20 @@ public class UserController {
     }
 
     @GetMapping("/trial/{id}")
-    public User getUserByIdTrial(@PathVariable Long id) {
-        return userService.getUserByID(id);
-
-        /*return "{\"id\":7," +
+    public String getUserByIdTrial(@PathVariable Long id) {
+        return "{\"id\":7," +
                 "\"personalInfo\":{" +
-                    "\"id\":12,\"location\":{" +
-                    "\"id\":13,\"name\":\"Budapest\"}," +
-                    "\"username\":\"trialT\"," +
-                    "\"name\":\"TrialTibor\"," +
-                    "\"email\":\"tibor.trial@gmail.com\"," +
-                    "\"phoneNumber\":\"222-444\"," +
-                    "\"status\":\"active\"," +
-                    "\"role\":\"user\", }," +
+                "\"id\":12,\"location\":{" +
+                "\"id\":13,\"name\":\"Budapest\"}," +
+                "\"username\":\"trialT\"," +
+                "\"name\":\"TrialTibor\"," +
+                "\"email\":\"tibor.trial@gmail.com\"," +
+                "\"phoneNumber\":\"222-444\"," +
+                "\"status\":\"active\"," +
+                "\"role\":\"user\", }," +
+                "\"profilePicture\":\"profile/picture.jpg\"" +
                 "\"expertInfo\":{" +
+                "\"description\":\"I make very nice pictures of dogs.\"" +
                 "\"id\":8," +
                 "\"rating\":3," +
                 "\"jobCount\":7," +
@@ -50,13 +50,17 @@ public class UserController {
                 "\"professions\":[{" +
                 "\"name\":\"Kutyasétáltató\"," +
                 "\"category\":{\"name\":" +
-                "\"Házkörülimunkák\"," +
+                "\"Ház körüli munkák\"," +
                 "\"id\":77}}]," +
                 "\"locations\":[{" +
                 "\"id\":13," +
-                "\"name\":\"Budapest\"},{\"id\":88,\"name\":\"Dubai\"}]},\"registrationTime\":\"2022-01-13T10:42:49.685216\",\"lastLoginTime\":\"2022-01-17T10:42:49.685216\"}";    }
-*/
+                "\"name\":\"Budapest\"}," +
+                "{\"id\":88," +
+                "\"name\":\"Dubai\"}]}," +
+                "\"registrationTime\":\"2022-01-13T10:42:49.685216\"," +
+                "\"lastLoginTime\":\"2022-01-17T10:42:49.685216\"}";
     }
+
     @PostMapping("/new")
     public String addNewUser(@RequestBody RegisterForm form) {
         userService.addNewUser(form);
@@ -82,18 +86,83 @@ public class UserController {
         return userService.getAll();
     }
 
-    @GetMapping("/search")
+/*    @GetMapping("/search")
     public List<User> getAllExpertByProfessionAndLocation(@RequestParam(name = "location") Long locationID,
                                                           @RequestParam(name = "profession") Long professionID){
         System.out.println("location Id: " + locationID + ", profession Id: " + professionID);
         //TODO: Call the service which returns the ordered list of searched Users.
         return userService.getAllUsersByLocationAndProfession(locationID, professionID);
-    }
+    }*/
 
-    @PostMapping("/search")
-    public String afsdfa(@RequestBody String x) {
-        System.out.println(x);
-        return "good";
+    @GetMapping("/search-all-trial")
+    public String afsdfa() {
+        return  "[{\"id\":7," +
+                "\"personalInfo\":{" +
+                "\"id\":12,\"location\":{" +
+                "\"id\":13,\"name\":\"Budapest\"}," +
+                "\"username\":\"trialT\"," +
+                "\"name\":\"TrialTibor\"," +
+                "\"email\":\"tibor.trial@gmail.com\"," +
+                "\"phoneNumber\":\"222-444\"," +
+                "\"status\":\"active\"," +
+                "\"role\":\"user\"," +
+                "\"profilePicture\":\"profile/picture.jpg\"," +
+                "\"expertInfo\":{" +
+                "\"description\":\"I make very nice pictures of dogs.\"," +
+                "\"id\":8," +
+                "\"rating\":3," +
+                "\"jobCount\":7," +
+                "\"reference\":[{" +
+                "\"id\":89," +
+                "\"description\":\"A nice dog I've walked\"," +
+                "\"imagePath\":\"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fsd5TBJe8TkI%2Fhqdefault.jpg&f=1&nofb=1\"" +
+                "}]," +
+                "\"professions\":[{" +
+                "\"name\":\"Kutyasétáltató\"," +
+                "\"category\":{\"name\":" +
+                "\"Ház körüli munkák\"," +
+                "\"id\":77}}]," +
+                "\"locations\":[{" +
+                "\"id\":13," +
+                "\"name\":\"Budapest\"}," +
+                "{\"id\":88," +
+                "\"name\":\"Dubai\"}]}," +
+                "\"registrationTime\":\"2022-01-13T10:42:49.685216\"," +
+                "\"lastLoginTime\":\"2022-01-17T10:42:49.685216\"}}," +
+                // Second User
+                "{\"id\":8," +
+                "\"personalInfo\":{" +
+                "\"id\":12,\"location\":{" +
+                "\"id\":13,\"name\":\"Budapest\"}," +
+                "\"username\":\"trialT\"," +
+                "\"name\":\"TrialTibor\"," +
+                "\"email\":\"tibor.trial@gmail.com\"," +
+                "\"phoneNumber\":\"222-444\"," +
+                "\"status\":\"active\"," +
+                "\"role\":\"user\"," +
+                "\"profilePicture\":\"profile/picture.jpg\"," +
+                "\"expertInfo\":{" +
+                "\"description\":\"I make very nice pictures of dogs.\"," +
+                "\"id\":8," +
+                "\"rating\":3," +
+                "\"jobCount\":7," +
+                "\"reference\":[{" +
+                "\"id\":89," +
+                "\"description\":\"A nice dog I've walked\"," +
+                "\"imagePath\":\"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fsd5TBJe8TkI%2Fhqdefault.jpg&f=1&nofb=1\"" +
+                "}]," +
+                "\"professions\":[{" +
+                "\"name\":\"Kutyasétáltató\"," +
+                "\"category\":{\"name\":" +
+                "\"Ház körüli munkák\"," +
+                "\"id\":77}}]," +
+                "\"locations\":[{" +
+                "\"id\":13," +
+                "\"name\":\"Budapest\"}," +
+                "{\"id\":88," +
+                "\"name\":\"Dubai\"}]}," +
+                "\"registrationTime\":\"2022-01-13T10:42:49.685216\"," +
+                "\"lastLoginTime\":\"2022-01-17T10:42:49.685216\"}}]";
     }
 
 }
