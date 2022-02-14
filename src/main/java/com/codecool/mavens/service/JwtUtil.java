326cd66c923d6d31dc1c80de1c232be44b4a46eb
keresztenymarcell <kereszteny.marcell@gmail.com>
2022-02-14 15:12:21 +0100

    feat: add JwtUtil helper class

---
+++ src/main/java/com/codecool/mavens/service/JwtUtil.java
@@ -0,0 +1,41 @@
+package com.codecool.mavens.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.sql.Ref;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "secretsecretsecretsecretsecretsecretsecret";
+    private static final int REFRESH_EXPIRATION = 14 * 24 * 60 * 60 * 1000;
+    private static final int ACCESS_EXPIRATION = 30 * 60 * 1000;
+
+    private Algorithm generateAlgorithm(){
+        return Algorithm.HMAC256(SECRET_KEY.getBytes());
+    }
+
+    public String generateAccessToken(User user, String url){
+        return JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + ACCESS_EXPIRATION))
+                .withIssuer(url)
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(generateAlgorithm());
+    }
+    public String generateRefreshToken(User user, String url){
+        return JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + REFRESH_EXPIRATION))
+                .withIssuer(url)
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(generateAlgorithm());
+    }
+
+
+}
