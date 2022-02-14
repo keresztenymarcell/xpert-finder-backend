a8820ea1b52c51ec61526056590a2775a287b09c
keresztenymarcell <kereszteny.marcell@gmail.com>
2022-02-14 15:57:32 +0100

    feat: add Filters to filter chain

--- src/main/java/com/codecool/mavens/security/SecurityConfig.java
+++ src/main/java/com/codecool/mavens/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.codecool.mavens.security;
 
 
-import com.codecool.mavens.service.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +10,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
@@ -37,11 +36,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(STATELESS)
                 .and()
+                .addFilter(new CustomAuthenticationFilter(authenticationManagerBean(), new JwtUtil()))
+                .addFilterAfter(new CustomAuthorizationFilter(new JwtUtil()), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests().antMatchers("/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilter(new CustomAuthenticationFilter(authenticationManagerBean(), new JwtUtil()));
-                ;
+                .anyRequest().authenticated();
 
     }
 
